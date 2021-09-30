3282fc4995e9760ec1655e19d9392628f5381165
Andrej Mazur <andrej.mazur@outlook.com>
2021-10-01 01:17:27 +0300

    fix: add fragment

---
+++ app/src/main/java/com/example/task5/ui/main/CatFragment.kt
@@ -0,0 +1,33 @@
+package com.example.task5.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.task5.databinding.FragmentCatBinding
+
+class CatFragment : Fragment() {
+
+    private var _binding: FragmentCatBinding? = null
+
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCatBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        fun create() = CatFragment()
+    }
+}
